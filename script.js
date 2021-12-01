933afc2733f16a09108e61026b9e9ad48e0140b9
Austin Thornley <austinjt1012@gmail.com>
2021-12-01 15:11:50 -0700

    Added sort button to sort movements

--- script.js
+++ script.js
@@ -6,22 +6,27 @@ const user = {
   name: 'Austin',
   username: 'at',
   pin: 1012,
-  movements: [400, 500, 200, -300, -200, 1200, -500, 900],
+  // movements: [400, 500, 200, -300, -200, 1200, -500, 900],
 };
+
+const allMovements = user.movements;
 
 const button = document.getElementById('button');
 const logout = document.getElementById('logout');
+const sort = document.getElementById('sort');
 const movementsEl = document.getElementById('movements');
 const userEl = document.getElementById('user');
 const pinEl = document.getElementById('pin');
 
-// change ui on login
+// add logout button when loggedin
 const updateLoginUI = function () {
   logout.style.opacity = 100;
+  sort.style.opacity = 100;
 };
 
+// function to display all transacations
 const displayMovements = function () {
-  const movementsHeading = `<h2 class="movements-header">Movements<h2>`;
+  const movementsHeading = `<h2 class="movements-header" id="movement-title">Movements<h2>`;
   movementsEl.insertAdjacentHTML('beforebegin', movementsHeading);
   movements.forEach(movement => {
     const html = `	
@@ -29,10 +34,20 @@ const displayMovements = function () {
 		`;
     movementsEl.insertAdjacentHTML('afterbegin', html);
   });
-
-  updateLoginUI();
 };
 
+const sortMovements = function (movements) {
+  const sortedMovements = movements.sort((a, b) => {
+    if (a > b) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  displayMovements(sortedMovements);
+};
+
+// function to verify user credentials
 const verifyUser = function () {
   if (userEl.value === user.username && Number(pinEl.value) === user.pin) {
     // clear inputs
@@ -40,23 +55,29 @@ const verifyUser = function () {
     pinEl.value = '';
     userEl.blur();
     pinEl.blur();
-    displayMovements();
   } else {
     alert('Error logging in. Please try again');
   }
 };
 
+// function to logout user
 const logoutUser = function () {
-  movementsEl.innerHTML = '';
+  document.getElementById('movement-title').innerHTML = '';
+  movementsEl.textContent = '';
   logout.style.opacity = 0;
-  console.log('Logged out');
+  sort.style.opacity = 0;
 };
 
 // verifyuser and login on button click
 button.addEventListener('click', function () {
   verifyUser();
+  displayMovements();
+  updateLoginUI();
   // logout on button click
   logout.addEventListener('click', function () {
     logoutUser();
+  });
+  sort.addEventListener('click', function () {
+    sortMovements(movements);
   });
 });
